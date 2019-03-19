e94b2f2193a58e492f26ee08d3707708f1a34442
Gillian Goud <gillian.goud@hva.nl>
2019-03-19 18:24:58 +0100

    created project, tested hellow, added mdoule using angular console

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
-
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AthletesModule } from './athletes/athletes.module';
+
 
 @Module({
-  imports: [],
+  imports: [AthletesModule],
   controllers: [AppController],
   providers: [AppService]
 })
